e64addbbe6f1d45bce7919320747ed7dc5bee604
M7mmdgMeR7 <Mohalaamer@icloud.com>
2017-06-27 13:16:19 +0300

    Update main.php

--- src/bowrain/main.php
+++ src/bowrain/main.php
@@ -1,6 +1,8 @@
 <?php
 namespace bowrain;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 class main extends PluginBase{
 public function onEnable(){
  $this->getServer()->getLogger()->info("plugin enabled");
@@ -9,6 +11,11 @@ public function onEnable(){
 public function onDisable(){
  $this->getServer()->getLogger()->info("plugin off");
  }
+public function onCommand(CommandSender $sender, Command $cmd, $label array $args){
+ switch($cmd->getName())}
+ case 'heal':
+ $sender->setHealth(20);
+ break;
 }
 
 
